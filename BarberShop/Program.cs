b6998cc17bd8f3b00ef9f32900807801f74f287a
Kevin Nunez <nunezortizkevin@gmail.com>
2026-05-26 20:35:27 -0400

    hola mundo

--- BarberShop/Program.cs
+++ BarberShop/Program.cs
@@ -10,9 +10,18 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// 1. Registramos los servicios de roles y autenticación
 builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+// 2. CORRECCIÓN AQUÍ: Registramos el CustomAuthenticationStateProvider de forma que 
+// pueda ser inyectado tanto por su clase concreta como por su clase base.
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+    provider.GetRequiredService<CustomAuthenticationStateProvider>());
+
+// 3. Agregamos el núcleo de autorización de Blazor
 builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
